2a06ad4cd8994dc11210ebb3fc30c76acf43ef2b
ranggatw <rangga.public@gmail.com>
2021-01-08 19:58:35 +0700

    test skill xapiens-refactory

--- soal-1.js
+++ soal-1.js
@@ -1,5 +1,10 @@
-let num = [1, 2, 3, 4, 5, 6, 7];
 
-for (let i = 0; i < num.length; i++) {
-  console.log(num[i])
+let num = [1, 2, 3, 4, 5, 6, 7];
+for (let i = 0; i <= num.length; i++) {
+  let add = num[i]
+  for (let j = 0; j <= num.length - (i + 2); j ++) {
+    add += ' 0'
+  }
+  console.log(add)
 }
+
